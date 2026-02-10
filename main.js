b583147b63f727f8941391bb33db55fca39e5e97
juwanyun28 <juwanyun28@gmail.com>
2026-02-10 10:56:15 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Add JS here
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'RvHFlqJTfjE',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    document.getElementById('playButton').addEventListener('click', function() {
+        player.playVideo();
+    });
+}
